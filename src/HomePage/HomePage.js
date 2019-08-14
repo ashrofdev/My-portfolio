48c44783b3c20bbda6b66fbb9dcd1f6108196f8c
First Last <salmanashraf853@gmail.com>
2019-08-15 00:17:04 +0100

    creating buttons component

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './home.css'
+import { Btn } from '../LittleComponents/LittleComponents';
 
 const HomePage = () => {
     return (
         <div className="home">
             Welcome to my Home Page
             <p>just started building the portfolio</p>
+            <Btn text={'DOWNLOAD NOW'}/>
         </div>
     )
 }
---
+++ src/LittleComponents/LittleComponents.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import './lc.css'
+
+export const Btn = ({text}) => {
+    return <button className="btn">{text}</button>
+}
