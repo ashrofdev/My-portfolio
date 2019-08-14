a3ef0dcf4ad41545a837331345e343410c598d1d
First Last <salmanashraf853@gmail.com>
2019-08-15 00:02:11 +0100

    starting

---
+++ src/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './home.css'
+
+const HomePage = () => {
+    return (
+        <div className="home">
+            Welcome to my Home Page
+            <p>just started building the portfolio</p>
+        </div>
+    )
+}
+export default HomePage
