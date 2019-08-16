26592dc7a6f9ce7d85afc72b6fd0bd5ffcdf0a69
First Last <salmanashraf853@gmail.com>
2019-08-16 08:08:30 +0100

    snaping the website to grid

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,18 +1,11 @@
 import React from 'react'
 import './home.css'
-import { Btn, Heading, Logo } from '../LittleComponents/LittleComponents';
 import Header from '../LargeComponents/Header';
 
 const HomePage = () => {
     return (
         <div className="home">
             <Header/>
-            <Btn text={'DOWNLOAD NOW'}/>
-            <Heading text={'Testing the heading component'} />
-            <Heading text={'Creating another heading'} />
-            <Heading text={'Creating a new heading'} />
-            <Heading text={'Creating a heading'} />
-            <Logo/>
         </div>
     )
 }
