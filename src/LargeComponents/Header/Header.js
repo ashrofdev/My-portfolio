301ba0b91a2953eac2dc40a5da0e4dc687461177
First Last <salmanashraf853@gmail.com>
2019-08-18 13:51:45 +0100

    building the header

--- src/LargeComponents/Header/Header.js
+++ src/LargeComponents/Header/Header.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import './header.css'
+import {Logo} from '../../LittleComponents/LittleComponents' 
 
 const Header = () => {
     return (
         <div className="header">
-            'Header section'
-            <p>uigh tgilh trsrgistrgilargilrhragygreag</p>
+            <div className="content">
+                <div className="top">
+                    <Logo/>
+                    {'navigation'}
+                </div>
+                <div className="bottom">
+                    <div className="text">
+                        <h2>HEY! THERE</h2>
+                        <h1>I AM SMITH</h1>
+                        <h2>A PROFESSIONAL DEVELOPER</h2>
+                    </div>
+                    <div className="img">
+                        <img src={require('./slider/man2.png')} />
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
