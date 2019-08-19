c70a2afbfc967bc3c37ea94be954e6e2fd40c358
First Last <salmanashraf853@gmail.com>
2019-08-20 00:26:32 +0100

    starting portfolio section

--- src/LargeComponents/Work/Work.js
+++ src/LargeComponents/Work/Work.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import './work.css'
+import { Heading } from '../../LittleComponents/LittleComponents';
 
 const Work = () => {
     return (
         <div className="work">
-            My latest works
+            <Heading text={'CHECK MY LATEST WORKS'}/>
+            <p>
+            Lorem ipsum dolor sit amet consectetur 
+            adipisicing elit. Sunt minus iste a quod neque fugit explicabo
+             tempore pariatur repellat asperiores?
+            </p>
         </div>
     )
 }
