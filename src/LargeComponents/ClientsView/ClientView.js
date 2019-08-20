f2f832e0c1984c5d0511f179caea48a4c4388b6e
First Last <salmanashraf853@gmail.com>
2019-08-20 14:24:43 +0100

    starting clients view section

--- src/LargeComponents/ClientsView/ClientView.js
+++ src/LargeComponents/ClientsView/ClientView.js
@@ -4,7 +4,22 @@ import './client.css'
 const ClientView = () => {
     return (
         <div className="client">
-            clients view of my works
+            <div className="person">
+                <div className="img">
+                    <img alt=""src={require('./testmonial/t1.png')} />
+                </div>
+                <div className="complement">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Neque officiis repellendus voluptas exercitationem numquam 
+                    modi? Quisquam aliquid consequatur ipsa adipisci.
+                </div>
+                <div className="name">
+                    JOHN DOE
+                </div>
+                <div className="job">
+                    BUssiness man
+                </div>
+            </div>
         </div>
     )
 }
