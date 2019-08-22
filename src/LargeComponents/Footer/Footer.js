d6170a95ae81c76aaee99c94fb0f3e94bb02e64d
First Last <salmanashraf853@gmail.com>
2019-08-22 06:42:01 +0100

    starting footer

--- src/LargeComponents/Footer/Footer.js
+++ src/LargeComponents/Footer/Footer.js
@@ -4,7 +4,23 @@ import './footer.css'
 const Footer = () => {
     return (
         <div className="footer">
-           Footer
+           <div className="det">
+            <img src={require('../../img/logo/logo.png')} />
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Neque officiis repellendus voluptas exercitationem numquam 
+                modi? Quisquam aliquid consequatur ipsa adipisci.
+            </p>
+            <div className="foot">
+                <span>&copy; All RIghts Reserved By Smarto</span>
+                <div className="icons">
+                    <i className="fa fa-facebook"></i>
+                    <i className="fa fa-twitter"></i>
+                    <i className="fa fa-pinterest"></i>
+                    <i className="fa fa-google"></i>
+                </div>
+            </div>
+           </div>
         </div>
     )
 }
