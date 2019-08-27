dcf57493511937e65a158758de2e2bf7a8bf308f
First Last <salmanashraf853@gmail.com>
2019-08-27 19:27:24 +0100

    building the left side of the features section

--- src/ServicesPage.js/Service.js
+++ src/ServicesPage.js/Service.js
@@ -10,7 +10,22 @@ const ServicesPage = () => {
                 <img src={require('../img/services/s1.jpg')} />
             </div>
             <div className="features">
-                features
+                <div className="ex">
+                    <span>Our</span> Exclusive 
+                    <div>Services <span>For You</span></div>
+                    <p>
+                    Lorem, ipsum dolor sit amet consectetur 
+                    adipisicing elit. Fugit praesentium earum non sit 
+                    sint ipsum.
+                    </p>
+                    <p>
+                    Lorem, ipsum dolor sit amet consectetur 
+                    adipisicing elit. Fugit praesentium.
+                    </p>
+                </div>
+                <div className="feas">
+                    features
+                </div>
             </div>
         </div>
     )
