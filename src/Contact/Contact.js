fa986b7dc89f2846c350ad5f4d47f248df2300f0
First Last <salmanashraf853@gmail.com>
2019-08-27 05:27:14 +0100

    building the info part

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -10,7 +10,12 @@ const ContactPage = () => {
             <div className="map">
                 <img src={require('../img/map.png')}/>
             </div>
-            
+            <div className="info">
+                <div className="headin">
+                    <span>Want To Connect</span> with Us 
+                    <div>Here is Our <span>Contact Information</span></div>
+                </div>
+            </div>
         </div>
     )
 }
